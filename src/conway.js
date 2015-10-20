542e5fcbad87072b9ecfdda3ffb9cf30604f2a41
jocelo <gopher.ae@gmail.com>
2015-10-20 14:26:05 -0500

    Project initialization

---
+++ src/conway.js
@@ -0,0 +1,16 @@
+var conway = (function() {
+    var rules = {},
+    init = function() {
+        
+    },
+    run = function() {
+        console.log("starting");
+    },
+    testo = function() {
+        return test;
+    };
+    return {
+        run:run,
+        testo:testo
+    };
+})();
