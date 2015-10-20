ad64b3cc795d7bc16ec6e6ad68f492bd577d01d9
jocelo <gopher.ae@gmail.com>
2015-10-20 15:05:38 -0500

    canvas draw

--- src/conway.js
+++ src/conway.js
@@ -1,15 +1,48 @@
 var conway = (function() {
-    var rules = {},
-    init = function() {
+    var rules={},
+    ctx,
+    cvs,
+    genAlive,
+    init = function(canvas, context) {
+        ctx = context;
+        cvs = canvas;
+        resizeCanvas();
+        drawSpace();
+        window.addEventListener('resize', resizeCanvas, false);
+    },
+    resizeCanvas = function() {
+        // resize to 90%
+        cvs.width = window.innerWidth-(window.innerWidth*.05);
+        cvs.height = window.innerHeight-(window.innerHeight*.05);
+    },
+    drawSpace = function() {
+        ctx.beginPath();
+        ctx.fill();
+        
+        // vertical lines
+        for (var i=0 ; i<cvs.width ; i+=10) {
+            ctx.moveTo(i, 0);
+            ctx.lineTo(i, cvs.height);
+        }
+        
+        // horizontal lines
+        for (var i=0 ; i<cvs.height ; i+=10) {
+            console.log(i);
+            ctx.moveTo(0, i);
+            ctx.lineTo(cvs.width, i);
+        }
+        ctx.stroke();
         
     },
     run = function() {
-        console.log("starting");
+        
     },
     testo = function() {
         return test;
     };
+    
     return {
+        start: init,
         run:run,
         testo:testo
     };
